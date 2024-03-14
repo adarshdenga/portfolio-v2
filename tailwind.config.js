4054e029be56844241bb9f59b5f94e74bf82768c
mynameadarsh <adarshdenga@gmail.com>
2024-03-14 19:38:13 +0530

    New portfolio first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        ...colors,
+      },
+    },
+  },
+  plugins: [],
+}
+
