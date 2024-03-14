0f0dda4dd0de371b809b7b8fe8440b613bf527df
mynameadarsh <adarshdenga@gmail.com>
2024-03-14 19:57:53 +0530

    Added basic header and body components

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div className="flex flex-row w-full p-4 gap-4 text-center">
+      <div className="p-4 aspect-square bg-rose-300 rounded-full">AD</div>
+      <div className="p-4 w-full bg-rose-300 rounded-full">About me / Work</div>
+      <div className="p-4 w-2/5 bg-rose-300 rounded-full">Socials</div>
+    </div>
+  );
+}
