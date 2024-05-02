6789f95320f182ca6f38de1da126325ae3c96830
mynameadarsh <adarshdenga@gmail.com>
2024-05-02 22:45:11 +0200

    New explode animation

--- src/components/Body.tsx
+++
@@ -1,8 +0,0 @@
-export default function Body() {
-  return (
-    <div className="flex flex-row w-full h-full px-4 gap-4 text-center">
-      <div className="p-4 w-full h-full bg-rose-300 rounded-full">Image</div>
-      <div className="p-4 w-2/5 h-full bg-rose-300 rounded-full">Info</div>
-    </div>
-  );
-}
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,21 +1,13 @@
 export default function Menu() {
   return (
-    <div className="text-white font-barlow text-5xl w-1/5">
-      <div className="line flex flex-row justify-between">
-        <div className="word">NATHAN</div>
-        <div className="word">SMITH</div>
-      </div>
-      <div className="line flex flex-row justify-between">
-        <div className="word">DESIGN</div>
-        <div className="word">&</div>
-      </div>
-      <div className="line flex flex-row justify-between">
-        <div className="word">ART</div>
-        <div className="word">DIRECTION</div>
-      </div>
-      <div className="line flex flex-row justify-between">
-        <div className="word">→EMAIL</div>
-        <div className="word">→INSTA</div>
+    <div className="flex w-full h-full text-white font-barlow text-5xl overflow-hidden justify-center items-center">
+      <div className="flex explode-container">
+        <span className="explode">D</span>
+        <span className="explode">E</span>
+        <span className="explode">S</span>
+        <span className="explode">I</span>
+        <span className="explode">G</span>
+        <span className="explode">N</span>
       </div>
     </div>
   );
